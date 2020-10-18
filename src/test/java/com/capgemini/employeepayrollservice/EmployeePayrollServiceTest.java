603370cc4b3d2f65cfe1931b3ad1231d974c691e
Abhijeet Kumar Giri <71620663+123akg@users.noreply.github.com>
2020-10-19 01:17:33 +0530

    Update EmployeePayrollServiceTest.java

--- src/test/java/com/capgemini/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/capgemini/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -12,7 +12,7 @@ public class EmployeePayrollServiceTest {
 		EmployeePayrollData[] arrayOfEmployees = { 
 				new EmployeePayrollData(1, "Abhijeet Kumar Giri, 50000.0),
 				new EmployeePayrollData(2, "Amit Kumar", 50000.0),
-				new EmployeePayrollData(3, "apeti Ganesh", 50000.0) };
+				new EmployeePayrollData(3, "Rapeti Ganesh", 50000.0) };
 		EmployeePayrollServiceMain payrollServiceObject = new EmployeePayrollServiceMain(Arrays.asList(arrayOfEmployees));
 		payrollServiceObject.writeEmployeeDdate(IOService.FILE_IO);
 		Assert.assertEquals(3, payrollServiceObject.countEnteries(IOService.FILE_IO));
